2896cf7ad85e3a93124b9e2327ff7e479e726791
Rifat Khan <rifat6633@gmail.com>
2024-11-21 20:01:40 +0600

    feat: redirect to "/" page if no admin user is logged in.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,15 @@
 import ProgressBar from "@/components/providers/ProgressBar";
 import { Toaster } from "@/components/ui/sonner";
+import auth from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const currentUser = auth.getCurrentUser();
+
+  if (!currentUser) {
+    redirect("/");
+  }
+
   return (
     <>
       <ProgressBar>
