2b6db8328f61ad77e56abe8fd3c6aced297303e1
Rifat Khan <rifat6633@gmail.com>
2024-11-21 14:48:32 +0600

    feat: Check for valid database and admin user. If admin user is not found then provide a registration form to register admin user.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { geistMono, geistSans } from "@/lib/fonts";
 import { ThemeProvider } from "@/features/dark-mode/theme-provider";
+import SetupAdminForm from "./_parts/SetupAdminForm";
+import db from "@/data";
 
 export const metadata: Metadata = {
   title: "SuperPress",
@@ -14,18 +16,42 @@ export const metadata: Metadata = {
   manifest: "/site.webmanifest",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const dbExist = await db.check.checkDatabaseConnection();
+  const adminExist = await db.check.checkSiteAdmin();
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
+          {dbExist ? (
+            adminExist ? (
+              <>{children}</>
+            ) : (
+              <main className="container mx-auto py-4">
+                <SetupAdminForm />
+              </main>
+            )
+          ) : (
+            <main className="container mx-auto flex h-screen items-center justify-center p-4">
+              <div className="max-w-xl">
+                <h1 className="text-2xl text-red-600">
+                  Database connection failed.
+                </h1>
+                <p className="text-foreground/60">
+                  Provide a valid database connection url in{" "}
+                  <code className="font-mono">.env</code> file and restart the
+                  server.
+                </p>
+              </div>
+            </main>
+          )}
         </ThemeProvider>
       </body>
     </html>
---
+++ src/data/checkDB.ts
@@ -0,0 +1,46 @@
+import prisma from "@/lib/prismadb";
+
+/**
+ * Checks the database connection by attempting to connect to the database.
+ * @returns true if the database connection is successful, false otherwise.
+ */
+export async function checkDatabaseConnection() {
+  try {
+    // Try to connect to the database
+    await prisma.$connect();
+    console.log("Database connection successful!");
+    return true;
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    return false;
+  }
+}
+
+/**
+ * Checks if the site admin exists by querying the database for a user with the "admin" capability.
+ * @returns true if the site admin exists, false otherwise.
+ */
+export async function checkSiteAdmin() {
+  try {
+    const user = await prisma.users.findFirst({
+      where: {
+        usermeta: {
+          every: {
+            key: "capability",
+            value: "admin",
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      console.error("Admin not found");
+      return false;
+    }
+    console.log("Admin found");
+    return true;
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    return false;
+  }
+}
