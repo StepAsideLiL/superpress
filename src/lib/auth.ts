4b3a45c5e9fb5031c98759b0659a1480a7d98238
Rifat Khan <rifat6633@gmail.com>
2024-11-21 15:59:00 +0600

    feat: create auth related functions.

---
+++ src/lib/auth.ts
@@ -0,0 +1,182 @@
+import "server-only";
+
+import {
+  encodeBase32LowerCaseNoPadding,
+  encodeHexLowerCase,
+} from "@oslojs/encoding";
+import { sha256 } from "@oslojs/crypto/sha2";
+import type { Users, Sessions } from "@prisma/client";
+import prisma from "@/lib/prismadb";
+import { cookies } from "next/headers";
+import { cache } from "react";
+
+type SessionValidationResult =
+  | { session: Sessions; user: Users }
+  | { session: null; user: null };
+
+/**
+ * Generate a random token for the session id.
+ * @returns Token for the session id.
+ */
+function generateSessionToken(): string {
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  const token = encodeBase32LowerCaseNoPadding(bytes);
+  return token;
+}
+
+/**
+ * Creates a new session and store in the db for the user.
+ * @param token Token for the session id from generateSessionToken() function.
+ * @param userId Id of the user.
+ * @returns Session object.
+ */
+async function createSession(token: string, userId: bigint): Promise<Sessions> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const session: Sessions = {
+    id: sessionId,
+    user_id: userId,
+    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+  };
+  await prisma.sessions.create({
+    data: session,
+  });
+  return session;
+}
+
+/**
+ * Validates the session token and extands the expiration date.
+ * @param token Token for the session id stored in the cookie.
+ * @returns Session object and user object.
+ */
+async function validateSessionToken(
+  token: string
+): Promise<SessionValidationResult> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const result = await prisma.sessions.findUnique({
+    where: {
+      id: sessionId,
+    },
+    include: {
+      user: true,
+    },
+  });
+  if (result === null) {
+    return { session: null, user: null };
+  }
+  const { user, ...session } = result;
+  if (Date.now() >= session.expiresAt.getTime()) {
+    await prisma.sessions.delete({ where: { id: sessionId } });
+    return { session: null, user: null };
+  }
+  if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+    await prisma.sessions.update({
+      where: {
+        id: session.id,
+      },
+      data: {
+        expiresAt: session.expiresAt,
+      },
+    });
+  }
+  return { session, user };
+}
+
+/**
+ * Invalidate (delete from the db) the session for the given session id.
+ * @param sessionId Id of the session to be invalidated.
+ */
+async function invalidateSession(sessionId: string) {
+  await prisma.sessions.delete({ where: { id: sessionId } });
+}
+
+/**
+ * Set the session token cookie for the user.
+ * @param token Token for the session id from generateSessionToken() function.
+ * @param expiresAt Expiration date for the session from createSession(token, userId) function.
+ */
+function setSessionTokenCookie(token: string, expiresAt: Date) {
+  const cookieStore = cookies();
+  cookieStore.set("sp-session", token, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    expires: expiresAt,
+    path: "/",
+  });
+}
+
+/**
+ * Delete the session token cookie.
+ */
+function deleteSessionTokenCookie() {
+  const cookieStore = cookies();
+  cookieStore.set("sp-session", "", {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 0,
+    path: "/",
+  });
+}
+
+/**
+ * Get the current session and user from the cookie.
+ * @returns Session object and user object.
+ */
+const getCurrentSession = cache(async (): Promise<SessionValidationResult> => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("sp-session")?.value ?? null;
+  if (token === null) {
+    return { session: null, user: null };
+  }
+  const result = await validateSessionToken(token);
+  return result;
+});
+
+const getCurrentUser = cache(
+  async (): Promise<{
+    id: bigint;
+    username: string;
+    email: string;
+  } | null> => {
+    const { user } = await getCurrentSession();
+    if (user === null) {
+      return null;
+    }
+    return {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    };
+  }
+);
+
+/**
+ * Set a new session in the cookie and db for the user.
+ * @param userId Id of the user.
+ */
+async function setNewUserSession(userId: bigint) {
+  const token = generateSessionToken();
+  const session = await createSession(token, userId);
+  setSessionTokenCookie(token, session.expiresAt);
+}
+
+/**
+ * Delete the session from the cookie and db.
+ * @param sessionId Id of the session.
+ */
+async function deleteUserSession(sessionId: string) {
+  await invalidateSession(sessionId);
+  deleteSessionTokenCookie();
+}
+
+const auth = {
+  getCurrentSession,
+  getCurrentUser,
+  setNewUserSession,
+  deleteUserSession,
+};
+
+export default auth;
