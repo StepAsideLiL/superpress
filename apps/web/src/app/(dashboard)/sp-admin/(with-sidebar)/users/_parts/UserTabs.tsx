9d4e6b68d2b565d054969d2574e61cf71619668a
Rifat Khan <rifat6633@gmail.com>
2024-11-30 15:36:44 +0600

    feat: change search param on tab change.

--- apps/web/src/app/(dashboard)/sp-admin/(with-sidebar)/users/_parts/UserTabs.tsx
+++ apps/web/src/app/(dashboard)/sp-admin/(with-sidebar)/users/_parts/UserTabs.tsx
@@ -1,37 +1,131 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { UserDataTableRowType } from "@/lib/types";
 import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-export default function UserTabs() {
+export default function UserTabs({ data }: { data: UserDataTableRowType[] }) {
+  const adminCount = data.filter((user) => user.role === "admin").length;
+  const editorCount = data.filter((user) => user.role === "editor").length;
+  const authorCount = data.filter((user) => user.role === "author").length;
+  const subscriberCount = data.filter(
+    (user) => user.role === "subscriber"
+  ).length;
+  const userCount = data.filter((user) => user.role === "user").length;
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  function handleTabClick(
+    tab: "all" | "admin" | "editor" | "author" | "subscriber" | "user"
+  ) {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (tab === "all") {
+      params.delete("role");
+    }
+
+    if (tab !== "all") {
+      params.set("role", tab);
+    }
+    router.push(pathname + "?" + params.toString());
+  }
+
   return (
     <div className="flex h-4 items-center gap-2">
-      <Button variant={"link"} className={cn("p-0 text-sm font-semibold")}>
-        All (2)
+      <Button
+        variant={"link"}
+        className={cn(
+          "p-0 text-sm",
+          searchParams.has("role") || "font-semibold"
+        )}
+        onClick={() => handleTabClick("all")}
+      >
+        All ({data.length})
       </Button>
 
-      <Separator orientation="vertical" className="bg-muted-foreground" />
+      {adminCount !== 0 && (
+        <>
+          <Separator orientation="vertical" className="bg-muted-foreground" />
+          <Button
+            variant={"link"}
+            className={cn(
+              "p-0 text-sm",
+              searchParams.get("role") === "admin" && "font-semibold"
+            )}
+            onClick={() => handleTabClick("admin")}
+          >
+            Admin ({adminCount})
+          </Button>
+        </>
+      )}
 
-      <Button variant={"link"} className={cn("p-0 text-sm")}>
-        Admin (2)
-      </Button>
+      {editorCount !== 0 && (
+        <>
+          <Separator orientation="vertical" className="bg-muted-foreground" />
+          <Button
+            variant={"link"}
+            className={cn(
+              "p-0 text-sm",
+              searchParams.get("role") === "editor" && "font-semibold"
+            )}
+            onClick={() => handleTabClick("editor")}
+          >
+            Editor ({editorCount})
+          </Button>
+        </>
+      )}
 
-      <Separator orientation="vertical" className="bg-muted-foreground" />
+      {authorCount !== 0 && (
+        <>
+          <Separator orientation="vertical" className="bg-muted-foreground" />
+          <Button
+            variant={"link"}
+            className={cn(
+              "p-0 text-sm",
+              searchParams.get("role") === "author" && "font-semibold"
+            )}
+            onClick={() => handleTabClick("author")}
+          >
+            Author ({authorCount})
+          </Button>
+        </>
+      )}
 
-      <Button variant={"link"} className={cn("p-0 text-sm")}>
-        Editor (2)
-      </Button>
+      {subscriberCount !== 0 && (
+        <>
+          <Separator orientation="vertical" className="bg-muted-foreground" />
+          <Button
+            variant={"link"}
+            className={cn(
+              "p-0 text-sm",
+              searchParams.get("role") === "subscriber" && "font-semibold"
+            )}
+            onClick={() => handleTabClick("subscriber")}
+          >
+            Subscriber ({subscriberCount})
+          </Button>
+        </>
+      )}
 
-      <Separator orientation="vertical" className="bg-muted-foreground" />
-
-      <Button variant={"link"} className={cn("p-0 text-sm")}>
-        Author (2)
-      </Button>
-
-      <Separator orientation="vertical" className="bg-muted-foreground" />
-
-      <Button variant={"link"} className={cn("p-0 text-sm")}>
-        Subscriber (2)
-      </Button>
+      {userCount !== 0 && (
+        <>
+          <Separator orientation="vertical" className="bg-muted-foreground" />
+          <Button
+            variant={"link"}
+            className={cn(
+              "p-0 text-sm",
+              searchParams.get("role") === "user" && "font-semibold"
+            )}
+            onClick={() => handleTabClick("user")}
+          >
+            userCount ({userCount})
+          </Button>
+        </>
+      )}
     </div>
   );
 }
