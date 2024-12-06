a26ce83fce13446ca873c6d6b68121e921c20d14
Rifat Khan <rifat6633@gmail.com>
2024-12-06 22:32:17 +0600

    refactor: update PostTabs component to handle all functionality of PostSearchParamLink compnent.

--- apps/web/src/app/(backend)/(dashboard)/sp-admin/(with-sidebar)/posts/_parts/PostTabs.tsx
+++ apps/web/src/app/(backend)/(dashboard)/sp-admin/(with-sidebar)/posts/_parts/PostTabs.tsx
@@ -1,83 +1,107 @@
-import PostSearchParamLink from "@/components/sp-ui/PostSearchParamLink";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { quickEditRowIdAtom } from "@/lib/store";
 import { PostCountByStatus } from "@/lib/types";
 import { cn } from "@/lib/utils";
-import { useSearchParams } from "next/navigation";
+import { useSetAtom } from "jotai";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function PostTabs({
   postCountByStatus,
 }: {
   postCountByStatus: PostCountByStatus;
 }) {
+  const router = useRouter();
+  const pathname = usePathname();
   const searchParams = useSearchParams();
   const params = new URLSearchParams(searchParams.toString());
+  const setQuickEditRowId = useSetAtom(quickEditRowIdAtom);
+
+  function handleClick(
+    tab: "all" | "published" | "pending" | "draft" | "trash"
+  ) {
+    if (tab === "all") {
+      params.delete("post_status");
+    }
+
+    if (tab !== "all") {
+      params.set("post_status", tab);
+    }
+
+    router.push(pathname + "?" + params.toString());
+    setQuickEditRowId("");
+  }
 
   return (
     <div className="flex h-4 items-center gap-2">
-      <PostSearchParamLink
-        reset={true}
-        className={cn("text-sm", params.has("post_status") || "font-semibold")}
+      <Button
+        variant={"link"}
+        className={cn(
+          "p-0 text-sm",
+          params.has("post_status") || "font-semibold"
+        )}
+        onClick={() => handleClick("all")}
       >
         All ({postCountByStatus.all})
-      </PostSearchParamLink>
+      </Button>
       {postCountByStatus.published !== 0 && (
         <>
           <Separator orientation="vertical" className="bg-muted-foreground" />
-          <PostSearchParamLink
-            param="post_status"
-            value="published"
+          <Button
+            variant={"link"}
             className={cn(
-              "text-sm",
+              "p-0 text-sm",
               params.get("post_status") === "published" && "font-semibold"
             )}
+            onClick={() => handleClick("published")}
           >
             Published ({postCountByStatus.published})
-          </PostSearchParamLink>
+          </Button>
         </>
       )}
       {postCountByStatus.pending !== 0 && (
         <>
           <Separator orientation="vertical" className="bg-muted-foreground" />
-          <PostSearchParamLink
-            param="post_status"
-            value="pending"
+          <Button
+            variant={"link"}
             className={cn(
-              "text-sm",
+              "p-0 text-sm",
               params.get("post_status") === "pending" && "font-semibold"
             )}
+            onClick={() => handleClick("pending")}
           >
             Pending ({postCountByStatus.pending})
-          </PostSearchParamLink>
+          </Button>
         </>
       )}
       {postCountByStatus.draft !== 0 && (
         <>
           <Separator orientation="vertical" className="bg-muted-foreground" />
-          <PostSearchParamLink
-            param="post_status"
-            value="draft"
+          <Button
+            variant={"link"}
             className={cn(
-              "text-sm",
+              "p-0 text-sm",
               params.get("post_status") === "draft" && "font-semibold"
             )}
+            onClick={() => handleClick("draft")}
           >
             Draft ({postCountByStatus.draft})
-          </PostSearchParamLink>
+          </Button>
         </>
       )}
       {postCountByStatus.trash !== 0 && (
         <>
           <Separator orientation="vertical" className="bg-muted-foreground" />
-          <PostSearchParamLink
-            param="post_status"
-            value="trash"
+          <Button
+            variant={"link"}
             className={cn(
-              "text-sm",
+              "p-0 text-sm",
               params.get("post_status") === "trash" && "font-semibold"
             )}
+            onClick={() => handleClick("trash")}
           >
             Trash ({postCountByStatus.trash})
-          </PostSearchParamLink>
+          </Button>
         </>
       )}
     </div>
