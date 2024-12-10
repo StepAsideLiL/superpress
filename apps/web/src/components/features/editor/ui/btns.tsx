365482156e8ef4d6a3f3b37bb155bf7cbbc7b150
Rifat Khan <rifat6633@gmail.com>
2024-12-10 09:49:42 +0600

    feat: rotate plus icon 45 degree to make cross icon to indicate close button.

--- apps/web/src/components/features/editor/ui/btns.tsx
+++ apps/web/src/components/features/editor/ui/btns.tsx
@@ -3,21 +3,24 @@
 import { Button } from "@/components/ui/button";
 import { useAtom } from "jotai";
 import {
+  openComponentSidebarAtom,
   toggleComponentSidebarAtom,
   toggleSettingsSidebarAtom,
 } from "../store";
 import { PanelRight, Plus } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export function SaveButton() {
   return <Button>Save</Button>;
 }
 
 export function ToggleComponentsSidebar() {
+  const [open] = useAtom(openComponentSidebarAtom);
   const [, toggleSidebar] = useAtom(toggleComponentSidebarAtom);
 
   return (
     <Button size={"icon"} onClick={() => toggleSidebar()}>
-      <Plus />
+      <Plus className={cn("rotate-0 transition-all", open && "rotate-45")} />
     </Button>
   );
 }
