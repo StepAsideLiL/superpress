03a7faacb2281eeaa7eb299afc1508b7fc0dc268
Rifat Khan <rifat6633@gmail.com>
2024-12-14 14:02:33 +0600

    feat: edit content string.

--- apps/web/src/components/features/editor/components/EditableElement.tsx
+++ apps/web/src/components/features/editor/components/EditableElement.tsx
@@ -1,19 +1,48 @@
 "use client";
 
 import { useAtom } from "jotai";
-import { selectedElementIdForEditingAtom } from "../libs/store";
+import {
+  EditorElement,
+  headingTags,
+  selectedElementIdForEditingAtom,
+  selectElementAtom,
+} from "../libs/store";
+import { cn } from "@/lib/utils";
 
 export default function EditableElement({
-  id,
+  element,
   children,
 }: {
-  id: string;
+  element: EditorElement;
   children: React.ReactNode;
 }) {
   const [, setSelectedElementId] = useAtom(selectedElementIdForEditingAtom);
+  const [, setElement] = useAtom(selectElementAtom);
+
+  if (headingTags.includes(element.type) && !Array.isArray(element.content)) {
+    return (
+      <div
+        className="mx-auto w-fit"
+        onClick={() => setSelectedElementId(element.id)}
+      >
+        <input
+          className={cn(
+            "bg-background focus-within:outline-none",
+            element.className
+          )}
+          style={element.style}
+          value={element.content}
+          onChange={(e) => setElement({ ...element, content: e.target.value })}
+        />
+      </div>
+    );
+  }
 
   return (
-    <div className="mx-auto w-fit" onClick={() => setSelectedElementId(id)}>
+    <div
+      className="mx-auto w-fit"
+      onClick={() => setSelectedElementId(element.id)}
+    >
       {children}
     </div>
   );
