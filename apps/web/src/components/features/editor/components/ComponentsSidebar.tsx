6dfeea8bc8f05a0469f244331a6d34e092f06890
Rifat Khan <rifat6633@gmail.com>
2024-12-15 20:48:57 +0600

    feat: add search.

--- apps/web/src/components/features/editor/components/ComponentsSidebar.tsx
+++ apps/web/src/components/features/editor/components/ComponentsSidebar.tsx
@@ -5,11 +5,14 @@ import { addEditorElementAtom, openComponentSidebarAtom } from "../libs/store";
 import { Button } from "@/components/ui/button";
 import { CloseComponentsSidebar } from "./editor-ui/btns";
 import { nanoid } from "../libs/utils";
-import { componentGroups } from "../libs/components";
+import { SearchInput } from "./editor-ui/SearchInput";
+import { useState } from "react";
+import { componentGroups, components } from "../libs/components";
 
 export default function ComponentsSidebar() {
   const [open] = useAtom(openComponentSidebarAtom);
   const [, addEditorElement] = useAtom(addEditorElementAtom);
+  const [search, setSearch] = useState("");
 
   if (open) {
     return (
@@ -19,40 +22,91 @@ export default function ComponentsSidebar() {
             <h1>Components</h1>
             <CloseComponentsSidebar />
           </div>
-          <div className="space-y-4 px-4 py-2">
-            {componentGroups.map((group) => {
-              return (
-                <div key={group.name} className="space-y-2">
-                  <h2 className="text-sm font-medium">{group.name}</h2>
-                  <div className="grid grid-cols-3 gap-2">
-                    {group.components.map((component) => {
-                      return (
-                        <Button
-                          key={component.lebel}
-                          variant={"ghost"}
-                          className="flex h-auto flex-col items-center justify-center gap-2 [&_svg]:size-5"
-                          onClick={() =>
-                            addEditorElement({
-                              id: nanoid(),
-                              type: component.type,
-                              content: component.content
-                                ? component.content
-                                : "",
-                              style: component.style,
-                              className: component.className,
-                            })
-                          }
-                        >
-                          <span>{component.title}</span>
-                          <component.icon className="size-6" />
-                        </Button>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
-            })}
+
+          <div className="px-4">
+            <SearchInput
+              placeholder="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
           </div>
+
+          {search.trim() === "" ? (
+            <div className="space-y-4 px-4 py-2">
+              {componentGroups.map((group) => {
+                return (
+                  <div key={group.name} className="space-y-2">
+                    <h2 className="text-sm font-medium">{group.name}</h2>
+                    <div className="grid grid-cols-3 gap-2">
+                      {group.components.map((component) => {
+                        return (
+                          <Button
+                            key={component.lebel}
+                            variant={"ghost"}
+                            className="flex h-auto flex-col items-center justify-center gap-2 [&_svg]:size-5"
+                            onClick={() =>
+                              addEditorElement({
+                                id: nanoid(),
+                                type: component.type,
+                                content: component.content
+                                  ? component.content
+                                  : "",
+                                style: component.style,
+                                className: component.className,
+                              })
+                            }
+                          >
+                            <span>{component.title}</span>
+                            <component.icon className="size-6" />
+                          </Button>
+                        );
+                      })}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div className="space-y-4 px-4 py-2">
+              <div className="grid grid-cols-3 gap-2">
+                {components
+                  .filter((component) =>
+                    component.tags?.some((tag) =>
+                      tag.includes(search.toLowerCase().trim())
+                    )
+                  )
+                  .map((component) => (
+                    <Button
+                      key={component.lebel}
+                      variant={"ghost"}
+                      className="flex h-auto flex-col items-center justify-center gap-2 [&_svg]:size-5"
+                      onClick={() =>
+                        addEditorElement({
+                          id: nanoid(),
+                          type: component.type,
+                          content: component.content ? component.content : "",
+                          style: component.style,
+                          className: component.className,
+                        })
+                      }
+                    >
+                      <span>{component.title}</span>
+                      <component.icon className="size-6" />
+                    </Button>
+                  ))}
+              </div>
+
+              {components.filter((component) =>
+                component.tags?.some((tag) =>
+                  tag.includes(search.toLowerCase().trim())
+                )
+              ).length === 0 && (
+                <div className="w-full text-center text-muted-foreground">
+                  No Components
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </section>
     );
