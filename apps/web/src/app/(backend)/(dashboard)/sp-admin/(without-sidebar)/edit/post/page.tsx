5a16b6a167375a554777c4e35904bd92a7e430ce
Rifat Khan <rifat6633@gmail.com>
2024-12-22 17:46:41 +0600

    refactor: update Post not found.

--- apps/web/src/app/(backend)/(dashboard)/sp-admin/(without-sidebar)/edit/post/page.tsx
+++ apps/web/src/app/(backend)/(dashboard)/sp-admin/(without-sidebar)/edit/post/page.tsx
@@ -1,14 +1,40 @@
 import Editor from "@/components/features/editor/Editor";
+import fetch from "@/lib/fetchers";
 
-export default async function Page(
-  props: {
-    searchParams: Promise<{ id: string }>;
-  }
-) {
+export default async function Page(props: {
+  searchParams: Promise<{ id: string }>;
+}) {
   const searchParams = await props.searchParams;
+
+  if (!searchParams.id || searchParams.id === "") {
+    return (
+      <main>
+        <div className="p-10">
+          <h1 className="text-center text-xl text-muted-foreground">
+            Post not found.
+          </h1>
+        </div>
+      </main>
+    );
+  }
+
+  const post = await fetch.post.getPostsForEdit(searchParams.id);
+
+  if (!post) {
+    return (
+      <main>
+        <div className="p-10">
+          <h1 className="text-center text-xl text-muted-foreground">
+            Post not found.
+          </h1>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <>
-      <Editor postId={searchParams.id} />
+      <Editor post={post} />
     </>
   );
 }
