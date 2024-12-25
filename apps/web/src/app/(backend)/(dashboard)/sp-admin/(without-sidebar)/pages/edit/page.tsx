9c9672aea6debbd9c434ee629def7c5b4b793e42
Rifat Khan <rifat6633@gmail.com>
2024-12-25 22:39:26 +0600

    chore: remove unused files.

--- apps/web/src/app/(backend)/(dashboard)/sp-admin/(without-sidebar)/pages/edit/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Page(
-  props: {
-    searchParams: Promise<{ page: string }>;
-  }
-) {
-  const searchParams = await props.searchParams;
-  return <div>Edit Page {searchParams.page}</div>;
-}
