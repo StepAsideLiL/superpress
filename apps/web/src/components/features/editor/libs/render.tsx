48caef624e3a02320a8b2dabdc5fa233fb386827
Rifat Khan <rifat6633@gmail.com>
2025-01-08 22:27:32 +0600

    chore: remove unused functions, variables, and components.

--- apps/web/src/components/features/editor/libs/render.tsx
+++ apps/web/src/components/features/editor/libs/render.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { EditorElementType } from "./types";
-import EditableElement from "../components/EditableElement";
 
 export const renderHtml = (elements: EditorElementType[]): React.ReactNode => {
   return elements.map((element) => {
@@ -17,38 +16,6 @@ export const renderHtml = (elements: EditorElementType[]): React.ReactNode => {
         type,
         { key: id, style, className },
         renderHtml(content) // Recursively render children
-      );
-    }
-
-    return null;
-  });
-};
-
-export const renderHtmlForEditor = (
-  elements: EditorElementType[]
-): React.ReactNode => {
-  return elements.map((element) => {
-    const { id, type, content, style, className } = element;
-
-    // If content is a string, render it as text
-    if (typeof content === "string") {
-      return (
-        <EditableElement key={id} element={element}>
-          {React.createElement(type, { key: id, style, className }, content)}
-        </EditableElement>
-      );
-    }
-
-    // If content is an array, recursively render children
-    if (Array.isArray(content)) {
-      return (
-        <EditableElement key={id} element={element}>
-          {React.createElement(
-            type,
-            { key: id, style, className },
-            renderHtmlForEditor(content)
-          )}
-        </EditableElement>
       );
     }
 
